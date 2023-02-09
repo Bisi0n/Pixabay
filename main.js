4490bc1cd87407b554d3a44fba233a97ad39abef
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-02-09 13:51:34 +0100

    v1.1

---
+++ main.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('form');
+let input = document.querySelector('#input')
+let color = document.querySelector('#select-color')
+
+
+
+let apiKey = 'https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2'
