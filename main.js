0b45994df4892e340a5c3b81de0c0979e58c4058
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-02-09 22:43:44 +0100

    v1.0.2

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 let form = document.querySelector('form');
 let input = document.querySelector('#input')
 let color = document.querySelector('#select-color')
+let image = document.querySelector('#photo')
 
+//form on submit, eventprevent default.
 
+form.onsubmit = async event => {
+    event.preventDefault();
+
+    let respons= await fetch('https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2&q=yellow+flowers&image_type=photo');
+
+    let jsonRespons = await respons.json();
+
+    let result = jsonRespons.hits[0].previewURL;
+    //let result = jsonRespons.hits;
+    //img = src
+
+    photo = new image
+    image = result;
+
+    //document.createelement(img);
+
+}
 
 let apiKey = 'https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2'
+
+// async function searchPhoto(){
+    
+//     let respons= await fetch('https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2&q=yellow+flowers&image_type=photo');
+    
+//     let json = await respons.json();
+// }
+
+
+
