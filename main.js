97f2f190521d04af6be9164a3b988f3a63891af4
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-02-16 16:20:02 +0100

    V2.0

--- main.js
+++ main.js
@@ -2,34 +2,71 @@ let form = document.querySelector('form');
 let input = document.querySelector('#input')
 let color = document.querySelector('#select-color')
 let photoSection = document.querySelector('#photo')
+let button = document.querySelector('#submit')
+let previous = document.querySelector('#prev')
+let next = document.querySelector('#next')
+let apiKey = 'https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2'
+let counter = 1; //page 
+
+//Uncaught ReferenceError: search is not defined
+let search;
+
+
+
 
 //form on submit, eventprevent default.
-
 form.onsubmit = async event => {
-    event.preventDefault();
-
-    let respons= await fetch('https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2&q=yellow+flowers&image_type=photo');
-
-    let jsonRespons = await respons.json();
-
-    //let result = jsonRespons.hits[0].previewURL;
-    //LargeImageUrl = HD
-    
-
-    // let img = document.createElement('img');
-    // img.src = result;
-    // photoSection.appendChild(img);
-
-    
-    //all pictures
-    jsonRespons.hits.forEach(hit => {
-      let img = document.createElement('img');
-      img.src = hit.LargeImageUrl;
-      photoSection.appendChild(img);
-    }
-    );
-
+  event.preventDefault();
+  search = '&q=' + color.value + '+' + input.value;
+  displayImage(search);
 }
+
+async function displayImage(search){
+  let respons = await fetch(apiKey + search + '&page=' + counter + '&per_page=10&image_type=photo');
+  let jsonRespons = await respons.json();
+
+  photoSection.textContent = ''; //clear images when searching again
+  jsonRespons.hits.forEach(hit => {
+      let img = document.createElement('img');
+      img.src = hit.webformatURL;
+      photoSection.appendChild(img);
+      
+      // Create tags element
+      let tags = document.createElement('p');
+      tags.textContent = hit.tags;
+      photoSection.appendChild(tags);
+      
+      // Create photographer element
+      let photographer = document.createElement('p');
+      photographer.textContent = ' Taken by:' + hit.user;
+      photoSection.appendChild(photographer);
+    });
+};
+
+// Previous button
+previous.onclick = event => {
+  counter--;
+  displayImage(search);
+}
+
+// Next button
+next.onclick = event => {
+  counter++;
+  displayImage(search);
+}
+
+
+
+
+// let button = document.querySelector('button')
+// let counter = 0;
+
+// button.onclick = event => {
+//   counter++;
+//   alert('You have clicked ' + counter + ' times')
+// }
+
+
 
 //let apiKey = 'https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2'
 
