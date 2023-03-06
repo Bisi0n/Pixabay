8064c949fcdcd3e957ce63a5c2e93aadfe679d62
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-03-06 16:42:09 +0100

    Refined & page reset fix

--- main.js
+++ main.js
@@ -8,28 +8,27 @@ let next = document.querySelector('#next')
 let buttonContainer = document.querySelector('#buttonContainer')
 let apiKey = 'https://pixabay.com/api/?key=33504400-b87edb6ce041e4f4c84fb55a2'
 let counter = 1; //page 
-
-//Uncaught ReferenceError: search is not defined
-let search;
-
+let perPage = 10;
+let totalHits = 0;
+let pagesTotal = 0;
+let search = '';
 
 
 form.onsubmit = async event => {
   event.preventDefault();
-  search = '&q=' + input.value + '&colors=' + color.value; //Sökningen efter färg görs på fel sätt
-  displayImage(search);
+  search = '&q=' + input.value + '&colors=' + color.value;
+  counter = 1; //resets page on new search
+  displayImage();
   buttonContainer.style.display = 'grid';
 }
 
-
-
-async function displayImage(search) {
-  let respons = await fetch(apiKey + search + '&page=' + counter + '&per_page=10&image_type=photo');
+async function displayImage() {
+  let respons = await fetch(apiKey + search + '&page=' + counter + '&per_page=' + perPage + '&image_type=photo');
   let jsonRespons = await respons.json();
 
-  photoSection.textContent = ''; //clear images when searching again
-  jsonRespons.hits.forEach(hit => {
+  photoSection.textContent = '';
 
+  jsonRespons.hits.forEach(hit => {
     // Create container element for each image
     let container = document.createElement('div');
     container.classList.add('main')
@@ -50,32 +49,28 @@ async function displayImage(search) {
 
     // Append container to photoSection
     photoSection.appendChild(container);
-
-    //Divide images on pages
-    let perPage = 10;
-    let totalHits = jsonRespons.totalHits;
-    pagesTotal = Math.ceil(totalHits / perPage)
-
-
-    // First page
-    if (counter === 1) {
-      previous.disabled = true;
-    }
-
-    // Last page
-    if (counter === pagesTotal) {
-      next.disabled = true;
-    }
   });
-};
 
+  //Divide images on pages
+  totalHits = jsonRespons.totalHits;
+  pagesTotal = Math.ceil(totalHits / perPage);
 
+  // First page
+  if (counter === 1) {
+    previous.disabled = true;
+  }
+
+  // Last page
+  if (counter === pagesTotal) {
+    next.disabled = true;
+  }
+}
 
 // Previous button
 previous.onclick = event => {
   if (counter > 1) {
     counter--;
-    displayImage(search);
+    displayImage();
 
     next.disabled = false;
   }
@@ -88,7 +83,7 @@ previous.onclick = event => {
 next.onclick = event => {
   if (counter < pagesTotal) {
     counter++;
-    displayImage(search);
+    displayImage();
 
     previous.disabled = false;
   }
@@ -96,7 +91,3 @@ next.onclick = event => {
     next.disabled = true;
   }
 }
-
-
-
-
